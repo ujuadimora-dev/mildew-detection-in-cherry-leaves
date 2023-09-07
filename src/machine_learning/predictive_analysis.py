34745e29ea6b5c5300506191441fc68fff482f4a
ujuadimora-dev <>
2023-09-07 23:53:51 +0000

    Modify slugimore and predictive analysis.py

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -6,7 +6,6 @@ from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
 
-
 def plot_predictions_probabilities(pred_proba, pred_class):
     """
     Plot prediction probability results
@@ -14,12 +13,12 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
     prob_per_class = pd.DataFrame(
         data=[0, 0],
-        index={'Powdery mildew': 0, 'Healthy': 1}.keys(),
+        index={'Powdery_mildew': 0, 'Healthy': 1}.keys(),
         columns=['Probability']
     )
-    prob_per_class.loc[pred_class] = pred_proba
+    prob_per_class.loc[pred_class.lower()] = pred_proba  # Use lower() to ensure consistent index
     for x in prob_per_class.index.to_list():
-        if x not in pred_class:
+        if x.lower() != pred_class.lower():  # Use lower() for comparison
             prob_per_class.loc[x] = 1 - pred_proba
     prob_per_class = prob_per_class.round(3)
     prob_per_class['Diagnostic'] = prob_per_class.index
@@ -32,14 +31,16 @@ def plot_predictions_probabilities(pred_proba, pred_class):
         width=600, height=300, template='seaborn')
     st.plotly_chart(fig)
 
+# The rest of your code remains unchanged
+
 
 def resize_input_image(img, version):
     """
     Reshape image to average image size
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
-    my_image = np.expand_dims(img_resized, axis=0)/255
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.BILINEAR)  # Use 'BILINEAR' instead
+    my_image = np.expand_dims(img_resized, axis=0) / 255
 
     return my_image
 
@@ -49,17 +50,19 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/malaria_detector_model.h5")
+    model = load_model(f"outputs/{version}/mildew_detector_model.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'Parasitized': 0, 'Uninfected': 1}.items()}
+    target_map = {v: k for k, v in {'Healthy': 0, 'infected with powdery_mildew': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample cell is "
-        f"**{pred_class.lower()}** with malaria.")
+        f"The predictive analysis indicates the sample cherry leaf(s) is "
+        f"**{pred_class.lower()}**")
 
     return pred_proba, pred_class
+
+    
